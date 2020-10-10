7f6b0ee6fd7f7293b6672f390f29efd02d4dd119
gidin21@gmail.com <gidin21@gmail.com>
2020-10-10 16:30:48 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "gidin21-notes-app-upload"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: " https://bsgkfi59l7.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_oDFI9d9Hx",
+      APP_CLIENT_ID: "besunjhtnklh2ufbbvmrfrgq6",
+      IDENTITY_POOL_ID: "us-east-1:acde8a42-a14e-47b9-8188-3ae70357b136"
+    }
+  };
